a22c02304dc1ca0c6907a0e9e95cd762a122b909
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-22 18:56:11 +0600

    get products api modified for pagination and a post api created for send data with ids

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t2ujek6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,7 +28,16 @@ async function run() {
     const productCollection = client.db("emaJohnDB").collection("products");
     // get products
     app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      console.log(req.query);
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+
+      const result = await productCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
       res.send(result);
     });
 
@@ -36,6 +45,14 @@ async function run() {
     app.get("/totalProducts", async (req, res) => {
       const result = await productCollection.estimatedDocumentCount();
       res.send({ totalProducts: result });
+    });
+
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const query = { _id: { $in: objectIds } };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
